b0b38dc39722c2901207efbe322eb96bd84a4b99
Uche <coolucheofia@gmail.com>
2023-02-08 21:43:02 +0100

    feat: Add shell script file for personal use

---
+++ 0x00-shell_basics/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cp 0-current_working_directory 4-listmorefiles
+cp 0-current_working_directory 5-listfilesdigitonly
+cp 0-current_working_directory 6-firstdirectory
+cp 0-current_working_directory 7-movethatfile
+cp 0-current_working_directory 8-firstdelete
+cp 0-current_working_directory 9-firstdirdeletion
+cp 0-current_working_directory 10-back
+cp 0-current_working_directory 11-lists
+cp 0-current_working_directory 12-file_type
+cp 0-current_working_directory 13-symbolic_link
+cp 0-current_working_directory 14-copy_html

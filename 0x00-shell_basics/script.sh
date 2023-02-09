857e6d175d6a86bd9e667e595e834b05a0549b95
cofucan <coolucheofia@gmail.com>
2023-02-09 02:18:07 +0100

    update: Personal script file

--- 0x00-shell_basics/script.sh
+++ 0x00-shell_basics/script.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-cp 0-current_working_directory 4-listmorefiles
-cp 0-current_working_directory 5-listfilesdigitonly
-cp 0-current_working_directory 6-firstdirectory
-cp 0-current_working_directory 7-movethatfile
-cp 0-current_working_directory 8-firstdelete
-cp 0-current_working_directory 9-firstdirdeletion
-cp 0-current_working_directory 10-back
-cp 0-current_working_directory 11-lists
-cp 0-current_working_directory 12-file_type
-cp 0-current_working_directory 13-symbolic_link
-cp 0-current_working_directory 14-copy_html
+# cp 0-current_working_directory 4-listmorefiles
+# cp 0-current_working_directory 5-listfilesdigitonly
+# cp 0-current_working_directory 6-firstdirectory
+# cp 0-current_working_directory 7-movethatfile
+# cp 0-current_working_directory 8-firstdelete
+# cp 0-current_working_directory 9-firstdirdeletion
+# cp 0-current_working_directory 10-back
+# cp 0-current_working_directory 11-lists
+# cp 0-current_working_directory 12-file_type
+# cp 0-current_working_directory 13-symbolic_link
+# cp 0-current_working_directory 14-copy_html
+# cp 0-current_working_directory 100-lets_move
+# cp 0-current_working_directory 101-clean_emacs
+# cp 0-current_working_directory 102-tree
+# cp 0-current_working_directory 103-commas
+# cp 0-current_working_directory 104-school.mgc

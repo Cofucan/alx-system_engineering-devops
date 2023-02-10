800840cec74a7dfcf4a1d9c6726ae35ce0631305
cofucan <coolucheofia@gmail.com>
2023-02-10 05:09:20 +0100

    feat: Script to create multiple files

---
+++ 0x01-shell_permissions/script.sh
@@ -0,0 +1,14 @@
+!/bin/bash
+cp 0-iam_betty 1-who_am_i
+cp 0-iam_betty 2-groups
+cp 0-iam_betty 3-new_owner
+cp 0-iam_betty 4-empty
+cp 0-iam_betty 5-execute
+cp 0-iam_betty 6-multiple_permissions
+cp 0-iam_betty 7-everybody
+cp 0-iam_betty 8-James_Bond
+cp 0-iam_betty 9-John_Doe
+cp 0-iam_betty 10-mirror_permissions
+cp 0-iam_betty 11-directories_permissions
+cp 0-iam_betty 12-directory_permissions
+cp 0-iam_betty 13-change_group

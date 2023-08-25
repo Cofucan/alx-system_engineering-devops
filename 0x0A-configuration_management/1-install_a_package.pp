c3b41db8ce04dc0f04ad1c8eec3c524df4bf5903
Uche Ofia <coolucheofia@gmail.com>
2023-08-25 21:42:49 +0100

    fix: Use package resource type instead of exec

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 # Installs flask from pip3
 
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command => 'pip3 install flask==2.1.0',
-  creates => '/usr/local/bin/flask', # Path to check if Flask is installed
-  require => Package['python3-pip'], # Make sure pip3 is installed first
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }

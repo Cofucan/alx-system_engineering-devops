b67789ce32880c90d8639495faf2492270e65222
Uche Ofia <coolucheofia@gmail.com>
2023-08-30 20:10:11 +0100

    refactor

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,13 +1,37 @@
 # Installs and configures Nginx web server with a redirect
+include stdlib
 
-exec { 'nginx_server':
-  command => [
-    '/usr/bin/apt-get', '-y', 'update',
-    '/usr/bin/apt-get', '-y', 'install', 'nginx',
-    '/bin/echo', 'Hello World!', '>', '/var/www/html/index.nginx-debian.html',
-    '/bin/bash', '-c', 'redir="\\trewrite ^/redirect_me https://www.github.com/Cofucan permanent;"; /bin/sed -i "/listen 80 default_server/a $redir" /etc/nginx/sites-available/default',
-    '/usr/sbin/service', 'nginx', 'restart'
-  ],
-  path    => ['/usr/bin', '/bin', '/usr/sbin'],
-  require => Package['nginx'],
+$link = 'https://www.github.com/Cofucan'
+$content = "\trewrite ^/redirect_me/$ ${link} permanent;"
+
+exec { 'update packages':
+  command => 'apt-get update'
+}
+
+exec { 'restart nginx':
+  command => '/usr/sbin/service nginx restart',
+  require => Package['nginx']
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update packages']
+}
+
+file { '/var/www/html/welcome.html':
+  ensure  => 'present',
+  content => 'Hello World!',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root'
+}
+
+file_line { 'Set 301 redirection':
+  ensure   => 'present',
+  after    => 'server_name\ _;',
+  path     => '/etc/nginx/sites-available/default',
+  multiple => true,
+  line     => $content,
+  notify   => Exec['restart nginx'],
+  require  => File['/var/www/html/welcome.html']
 }

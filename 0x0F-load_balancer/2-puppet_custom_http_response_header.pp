51fbe8389ed87fe59ac2e186de70ca7e32307358
Uche Ofia <coolucheofia@gmail.com>
2023-09-05 23:17:16 +0100

    fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,49 +5,6 @@ package { 'nginx':
   ensure => 'installed',
 }
 
-# Create a custom index page
-file { '/var/www/html/uche.html':
-  ensure  => 'file',
-  content => 'Hello World!\n',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-}
-
-# Define Nginx server block for the default configuration
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => 'file',
-  content => "server {
-    listen 80;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index uche.html index.html index.htm index.nginx-debian.html;
-
-    location /redirect_me {
-        return 301 https://www.github.com/Cofucan;
-    }
-
-    error_page 404 /404.html;
-    location = /404.html {
-        root /var/www/html/;
-        internal;
-    }
-}",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Create a custom 404 page
-file { '/var/www/html/404.html':
-  ensure  => 'file',
-  content => "Ceci n'est pas une page\n",
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  notify  => Service['nginx'],
-}
-
 # Ensure custom HTTP header is added to Nginx config
 exec { 'add_custom_header':
   command     => "grep -q 'add_header X-Served-By \$hostname;' /etc/nginx/nginx.conf || " +

9c65dff9fdf6977e5de304a2cdb7455202313d48
Uche Ofia <coolucheofia@gmail.com>
2023-09-05 23:19:19 +0100

    fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,16 @@
 # Puppet manifest to configure Nginx with a custom 404 page and a redirection rule.
 
-# Install Nginx package
-package { 'nginx':
-  ensure => 'installed',
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-# Ensure custom HTTP header is added to Nginx config
-exec { 'add_custom_header':
-  command     => "grep -q 'add_header X-Served-By \$hostname;' /etc/nginx/nginx.conf || " +
-                "sudo sed -i '/^http {/a \\tadd_header X-Served-By \$hostname;' /etc/nginx/nginx.conf",
-  path        => '/usr/bin:/usr/sbin:/bin',
-  refreshonly => true,
-  subscribe   => File['/etc/nginx/sites-enabled/default'],
-  notify      => Service['nginx'],
+-> package {'nginx':
+  ensure => 'present',
 }
-
-# Ensure the Nginx service is running and enabled
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }

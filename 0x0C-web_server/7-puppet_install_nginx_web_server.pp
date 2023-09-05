8a6d9223022cb3b47eeb61218d0059d4b612d4fd
Uche Ofia <coolucheofia@gmail.com>
2023-09-05 11:11:44 +0100

    fix: Puppet manifest

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,37 +1,55 @@
-# Installs and configures Nginx web server with a redirect
-include stdlib
+# Puppet manifest to configure Nginx with a custom 404 page and a redirection rule.
 
-$link = 'https://www.github.com/Cofucan'
-$content = "\trewrite ^/redirect_me/$ ${link} permanent;"
-
-exec { 'update packages':
-  command => 'apt-get update'
-}
-
-exec { 'restart nginx':
-  command => '/usr/sbin/service nginx restart',
-  require => Package['nginx']
-}
-
+# Install Nginx package
 package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['update packages']
+  ensure => 'installed',
 }
 
-file { '/var/www/html/welcome.html':
-  ensure  => 'present',
+# Create a custom index page
+file { '/var/www/html/uche.html':
+  ensure  => 'file',
   content => 'Hello World!',
+  owner   => 'www-data',
+  group   => 'www-data',
   mode    => '0644',
-  owner   => 'root',
-  group   => 'root'
 }
 
-file_line { 'Set 301 redirection':
-  ensure   => 'present',
-  after    => 'server_name\ _;',
-  path     => '/etc/nginx/sites-available/default',
-  multiple => true,
-  line     => $content,
-  notify   => Exec['restart nginx'],
-  require  => File['/var/www/html/welcome.html']
+# Define Nginx server block for the default configuration
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => 'file',
+  content => "server {
+    listen 80;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html uche.html;
+
+    location /redirect_me {
+        return 301 https://www.github.com/Cofucan;
+    }
+
+    error_page 404 /404.html;
+    location = /404.html {
+        root /var/www/html/;
+        internal;
+    }
+}",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Create a custom 404 page
+file { '/var/www/html/404.html':
+  ensure  => 'file',
+  content => "Ceci n'est pas une page\n",
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
+  notify  => Service['nginx'],
+}
+
+# Ensure the Nginx service is running and enabled
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
 }
